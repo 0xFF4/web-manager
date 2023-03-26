e58d4e962cf9a5063786fd26cb6cc597afa5dc0c
dreadful <0xFF4@skids2themoon@proton.me>
2023-03-26 23:12:26 +0200

    created the `new` function which will start our new web instance & created the route handler.

--- core/web/Handler.go
+++ core/web/Handler.go
@@ -1,1 +1,24 @@
 package web
+
+import (
+	"manager/core/config"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// New will start a new web server instance
+func New() {
+
+	// Initialize a brand new router
+	handler := mux.NewRouter()
+
+	server := &http.Server{
+		Addr:    config.Options.String("server", "host"),
+		Handler: handler,
+	}
+
+	// Serve the HTTP server using TLS if it is enabled inside the configuration
+	server.ListenAndServe()
+
+}
